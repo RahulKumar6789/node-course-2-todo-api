36335cd4235842c2461f1b2552a777024e336a39
RahulKumar6789 <32266017+RahulKumar6789@users.noreply.github.com>
2017-10-05 19:23:26 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds163294.mlab.com:63294/mongoose' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
