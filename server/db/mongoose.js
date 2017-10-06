54703cc4c685bf2cb9370624b9b5d9cbfa48d12c
Rahul Kumar <rahul.kumar@sofocle.com>
2017-10-06 09:32:59 +0530

    modified mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds163294.mlab.com:63294/mongoose' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://rahul.kumar@sofocle.com:sofocle@1234@ds163294.mlab.com:63294/mongoose' || 'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
