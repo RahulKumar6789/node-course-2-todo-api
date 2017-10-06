ee580aea4b1e1d52f764abc3547e0308e00a30dc
Rahul Kumar <rahul.kumar@sofocle.com>
2017-10-06 17:06:58 +0530

    Setup seprate test and devlopment env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://rahul.kumar@sofocle.com:sofocle@1234@ds163294.mlab.com:63294/mongoose' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
